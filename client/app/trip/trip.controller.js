8bf53ac7e14f1cd61e06ce06f3e59373a3d22f5b
Amy Meng <amymeng5277@gmail.com>
2016-10-14 00:34:53 -0400

    [trip] most finish the form of posting a trip

--- client/app/trip/trip.controller.js
+++ client/app/trip/trip.controller.js
@@ -1,36 +1,97 @@
 'use strict';
 
-(function() {
+(function () {
 
-class TripController {
+  class TripController {
 
-  constructor($http, $scope, socket) {
-    this.$http = $http;
-    this.awesomeThings = [];
+    constructor($http, $scope, socket) {
+      this.$http = $http;
+      this.$scope = $scope;
+      this.awesomeThings = [];
 
-    $http.get('/api/things').then(response => {
-      this.awesomeThings = response.data;
-      socket.syncUpdates('thing', this.awesomeThings);
-    });
+      $http.get('/api/things').then(response => {
+        this.awesomeThings = response.data;
+        socket.syncUpdates('thing', this.awesomeThings);
+      });
 
-    $scope.$on('$destroy', function() {
-      socket.unsyncUpdates('thing');
-    });
-  }
+      $scope.$on('$destroy', function () {
+        socket.unsyncUpdates('thing');
+      });
 
-  addThing() {
-    if (this.newThing) {
-      this.$http.post('/api/things', { name: this.newThing });
-      this.newThing = '';
+      // for calendar
+      this.today();
+      this.toggleMin();
+      $scope.maxDate = new Date(2020, 5, 22);
+      $scope.dateOptions = {
+        formatYear: 'yy',
+        startingDay: 1
+      };
+      $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+      $scope.format = $scope.formats[0];
+      $scope.status_pickup = {opened: false};
+      $scope.status_arrive = {opened: false};
+      var tomorrow = new Date();
+      tomorrow.setDate(tomorrow.getDate() + 1);
+      var afterTomorrow = new Date();
+      afterTomorrow.setDate(tomorrow.getDate() + 2);
+      $scope.events = [{
+        date: tomorrow,
+        status: 'full'
+      }, {
+        date: afterTomorrow,
+        status: 'partially'
+      }];
+    }
+
+    today() {
+      this.$scope.arrive_dt = new Date();
+      this.$scope.pickup_dt = new Date();
+    }
+
+    clear() {
+      this.$scope.arrive_dt = null;
+      this.$scope.arrive_dt = null;
+    }
+
+    disabled(date, mode) {
+      return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
+    }
+
+    toggleMin() {
+      this.$scope.minDate = this.$scope.minDate ? null : new Date();
+    }
+
+    open($event, target) {
+      this.$scope['status_' + target].opened = true;
+    }
+
+    getDayClass(date, mode) {
+      if (mode === 'day') {
+        var dayToCheck = new Date(date).setHours(0, 0, 0, 0);
+        for (var i = 0; i < this.$scope.events.length; i++) {
+          var currentDay = new Date(this.$scope.events[i].date).setHours(0, 0, 0, 0);
+          if (dayToCheck === currentDay) {
+            return this.$scope.events[i].status;
+          }
+        }
+      }
+      return '';
+    }
+
+    addThing() {
+      if (this.newThing) {
+        this.$http.post('/api/things', {name: this.newThing});
+        this.newThing = '';
+      }
+    }
+
+    deleteThing(thing) {
+      this.$http.delete('/api/things/' + thing._id);
     }
   }
 
-  deleteThing(thing) {
-    this.$http.delete('/api/things/' + thing._id);
-  }
-}
-
-angular.module('uwece651f16NewApp')
-  .controller('TripController', TripController);
+  angular.module('uwece651f16NewApp')
+    .controller('TripController', TripController);
 
 })();
+

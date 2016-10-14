96f803c749d1f8823ea746770f29c86c4a1b7234
Amy Meng <amymeng5277@gmail.com>
2016-10-14 11:57:25 -0400

    [trip] Add trip and search button in navbar

--- client/app/trip/trip.controller.js
+++ client/app/trip/trip.controller.js
@@ -18,64 +18,7 @@
         socket.unsyncUpdates('thing');
       });
 
-      // for calendar
-      this.today();
-      this.toggleMin();
-      $scope.maxDate = new Date(2020, 5, 22);
-      $scope.dateOptions = {
-        formatYear: 'yy',
-        startingDay: 1
-      };
-      $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
-      $scope.format = $scope.formats[0];
-      $scope.status_pickup = {opened: false};
-      $scope.status_arrive = {opened: false};
-      var tomorrow = new Date();
-      tomorrow.setDate(tomorrow.getDate() + 1);
-      var afterTomorrow = new Date();
-      afterTomorrow.setDate(tomorrow.getDate() + 2);
-      $scope.events = [{
-        date: tomorrow,
-        status: 'full'
-      }, {
-        date: afterTomorrow,
-        status: 'partially'
-      }];
-    }
 
-    today() {
-      this.$scope.arrive_dt = new Date();
-      this.$scope.pickup_dt = new Date();
-    }
-
-    clear() {
-      this.$scope.arrive_dt = null;
-      this.$scope.arrive_dt = null;
-    }
-
-    disabled(date, mode) {
-      return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
-    }
-
-    toggleMin() {
-      this.$scope.minDate = this.$scope.minDate ? null : new Date();
-    }
-
-    open($event, target) {
-      this.$scope['status_' + target].opened = true;
-    }
-
-    getDayClass(date, mode) {
-      if (mode === 'day') {
-        var dayToCheck = new Date(date).setHours(0, 0, 0, 0);
-        for (var i = 0; i < this.$scope.events.length; i++) {
-          var currentDay = new Date(this.$scope.events[i].date).setHours(0, 0, 0, 0);
-          if (dayToCheck === currentDay) {
-            return this.$scope.events[i].status;
-          }
-        }
-      }
-      return '';
     }
 
     addThing() {

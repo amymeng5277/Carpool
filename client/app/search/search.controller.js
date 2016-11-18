a09a672376161babdb44ea89304f8a7c30f145c4
Howard Pang <howardpang28@gmail.com>
2016-11-18 00:41:08 -0500

    [Search] working on Fontend's search JS, mocked REST API call

--- client/app/search/search.controller.js
+++ client/app/search/search.controller.js
@@ -8,14 +8,71 @@ class SearchController {
     this.$http = $http;
     this.awesomeThings = [];
 
-    $http.get('/api/things').then(response => {
-      this.awesomeThings = response.data;
-      socket.syncUpdates('thing', this.awesomeThings);
-    });
+    // $http.get('/api/things').then(response => {
+    //   this.awesomeThings = response.data;
+    //   socket.syncUpdates('thing', this.awesomeThings);
+    // });
 
-    $scope.$on('$destroy', function() {
-      socket.unsyncUpdates('thing');
-    });
+    // $scope.$on('$destroy', function() {
+    //   socket.unsyncUpdates('thing');
+    // });
+
+    function createResultEntry(data, rowNumber) {
+      var table = $("#searchResultTable");
+      var row = $('<tr>', {id: 'row'+rowNumber});
+      var col1 = $('<td>'+data['pick-up']['city']+'<div class="expandDetail">'+data['pick-up']['address']+'</div></td>');
+      var col2 = $('<td>'+data['drop-off']['city']+'<div class="expandDetail">'+data['drop-off']['address']+'</div></td>');
+      var col3 = $('<td><button type="button" class="transparent-btn"><span class="glyphicon glyphicon-map-marker"></span></button></td>');
+      var col4 = $('<td>2/13/2016 4:00 PM<div class="expandDetail">In 48h 45m</div></td>');
+      var col5 = $('<td>3/4<div class="expandDetail"><img class="car-seats" src="assets/images/car.png"></div></td>');
+      var col6 = $('<td><i class="fa fa-female"></i> <i class="fa fa-music"></i> <i class="fa fa-wheelchair"></i></td>');
+
+      var col7 = $('<td>'+data['vehicle']['type']+'<div class="expandDetail">'+data['vehicle']['make']+' '+data['vehicle']['model']+' '+data['vehicle']['year']+'</div></td>')
+      var col8 = $('<td>\
+        <button type="button" ng-click="toggleInfo('+rowNumber+')" class="transparent-btn">\
+          <span id="arrow" class="glyphicon glyphicon-chevron-down"></span>\
+        </button>\
+        <button type="button" class="btn btn-xs btn-join">Join</button>\
+      </td>');
+
+      row.append(col1).append(col2).append(col3).append(col4).append(col5).append(col6).append(col7).append(col8);
+      table.append(row);
+    }
+
+    function populateSearchTable(results) {
+      for (var idx = 0; idx < results['result'].length; idx++) {
+        createResultEntry(results['result'][idx], idx+1);
+      }
+    }
+
+    $scope.doQuery = function() {
+      // clear previous search results
+      $("[id^=row]").remove();
+
+      // build search query, hardcoded url to local host (need to change)
+      var pickup = $('#pickup').val();
+      pickup = pickup.split(' ').join('+');
+      var dropoff = $('#dropoff').val();
+      dropoff = dropoff.split(' ').join('+');
+      var url = "http://localhost:9000/query";
+      url += "?pickup=" + pickup + "&dropoff=" + dropoff;
+      //console.log(url);
+
+      // restful call to get search results
+      var xhr = new XMLHttpRequest();
+      xhr.open("GET", url, false);
+      xhr.send();
+
+      if (xhr.status == 200) {
+        var result = JSON.parse(xhr.response);
+        if (result['result'].length == 0) {
+          $('#no-matches').show();
+          return;
+        }
+        $('#no-matches').hide();
+        populateSearchTable(result);
+      }
+    };
 
     $scope.toggleInfo = function(row) {
       // handle the arrow and determine if expanding or minimizing
@@ -39,16 +96,16 @@ class SearchController {
     };
   }
 
-  addThing() {
-    if (this.newThing) {
-      this.$http.post('/api/things', { name: this.newThing });
-      this.newThing = '';
-    }
-  }
+  // addThing() {
+  //   if (this.newThing) {
+  //     this.$http.post('/api/things', { name: this.newThing });
+  //     this.newThing = '';
+  //   }
+  // }
 
-  deleteThing(thing) {
-    this.$http.delete('/api/things/' + thing._id);
-  }
+  // deleteThing(thing) {
+  //   this.$http.delete('/api/things/' + thing._id);
+  // }
 }
 
 angular.module('uwece651f16NewApp')
--- server/routes.js
+++ server/routes.js
@@ -21,6 +21,82 @@ module.exports = function(app) {
 
   app.use('/auth', require('./auth'));
 
+  // ---BEGIN---
+  // The routing that was done previously doesn't match the API that was specified, so
+  // I created dummy ones here that don't actually connect with the DB.
+  app.route('/query')
+    .get(function(req, res) {
+      // this does a search based on to, from, and date
+      if (req.method == 'GET') {
+        var emptyResponse = {
+          'result': []
+        }
+        var dummyResponse = {
+          'result': [
+            {
+              'pick-up': {
+                'city': 'Waterloo',
+                'address': '201 King Street'
+              },
+              'drop-off': {
+                'city': 'Toronto',
+                'address': '3422 College Street'
+              },
+              'time': 1479440393,
+              'spots': 3,
+              'vehicle': {
+                'type': 'Sedan',
+                'make': 'Honda',
+                'model': 'Civic',
+                'year': 2016,
+                'seats': 4
+              },
+              'preferences': [
+                {'pref':'female'},
+                {'pref':'music'},
+                {'pref':'handicap'}
+              ]
+            },
+            {
+              'pick-up': {
+                'city': 'Missisauga',
+                'address': '100 City Center Dr.'
+              },
+              'drop-off': {
+                'city': 'New Market',
+                'address': '438 Park Ave'
+              },
+              'time': 1479440393,
+              'spots': 3,
+              'vehicle': {
+                'type': 'Sedan',
+                'make': 'Honda',
+                'model': 'Civic',
+                'year': 2016,
+                'seats': 4
+              },
+              'preferences': [
+                {'pref':'female'},
+                {'pref':'music'},
+                {'pref':'handicap'}
+              ]
+            }
+          ]
+        };
+        var displayEmpty = Math.random() > 0.5;
+        if (displayEmpty) {
+          res.write(JSON.stringify(emptyResponse));
+        } else {
+          res.write(JSON.stringify(dummyResponse));
+        }
+        res.end();
+      } else if (req.method == 'POST') {
+        
+      }
+    });
+
+  // ---END---
+
   // All undefined asset or api routes should return a 404
   app.route('/:url(api|auth|components|app|bower_components|assets)/*')
    .get(errors[404]);

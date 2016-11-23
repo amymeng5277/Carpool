2e1f989a98019142afbedf809f9c92620d41a633
Xuelun Wu <xue777hua@gmail.com>
2016-11-23 00:18:08 -0500

    [profile] reload profile page after update info.

--- client/app/app.constant.js
+++ client/app/app.constant.js
@@ -3,7 +3,7 @@
 
 angular.module('uwece651f16NewApp.constants', [])
 
-.constant('appConfig', {userRoles:['guest','user','admin']})
+.constant('appConfig', {userRoles:['guest','user','admin'],gmailAuth:{username:'ezcarpoolece651',clientId:'32088891866-u5856onrdfejoj8i8f6kqks12qo4b9jg.apps.googleusercontent.com',clientSecret:'FE0oHl_RNpQmwPUjYVwYfl6X',refreshToken:'1/Rw0mXQmg0Qg2t3XZ-fzF1g6bx5J6SgT_XJ4ADaTCyOg'}})
 
 ;
 })(angular);
--- client/app/profile/profile.controller.js
+++ client/app/profile/profile.controller.js
@@ -1,102 +1,98 @@
 'use strict';
 
-(function() {
+(function () {
 
-class ProfileController {
+  class ProfileController {
 
-  constructor($http, $scope, socket, Auth) {
-    this.$http = $http;
-    this.awesomeThings = [];
-    var _this = this;
+    constructor($http, $scope, socket, Auth, $state, $modal) {
+      this.$http = $http;
+      this.$state = $state;
+      this.$modal = $modal;
+      var _this = this;
 
-    Auth.getCurrentUser(function(user) {
-      _this.currentUser = user;
-      $http.get('/api/users/'+user._id+'/vehicles')
-        .success(response => {
-          _this.myVehicle1 = response[0];
-          _this.myVehicle2 = response[1];
-        });
-    });
-
-    $http.get('/api/things').then(response => {
-      this.awesomeThings = response.data;
-      socket.syncUpdates('thing', this.awesomeThings);
-    });
-
-    $scope.$on('$destroy', function() {
-      socket.unsyncUpdates('thing');
-    });
-  }
-
-  save() {
-  alert ("Save succeeded! \nPlease reload to check the updated profile information.");
-
-  if(this.currentUser._id) {
-      // update
-      this.$http.put('/api/users/'+this.currentUser._id + '/basicInfo', this.currentUser)
-        .success(function(response){
-          console.log(response);
-        });
-
-    } else {
-      this.driverId = this.currentUser.driver_id;
-      this.$http.post('/api/users/basicInfoCreat', this.currentUser)
-        .success(function(response){
-          console.log(response);
-        });
+      Auth.getCurrentUser(function (user) {
+        _this.currentUser = user;
+        $http.get('/api/users/' + user._id + '/vehicles')
+          .success(response => {
+            _this.myVehicle1 = response[0];
+            _this.myVehicle2 = response[1];
+          });
+      });
     }
 
-   if(this.myVehicle1 && this.myVehicle1._id) {
-      // update
-      this.$http.put('/api/vehicles/'+this.myVehicle1._id, this.myVehicle1)
-        .success(function(response){
-          console.log(response);
-        });
+    save() {
+      if (this.currentUser._id) {
+        // update
+        this.$http.put('/api/users/' + this.currentUser._id + '/basicInfo', this.currentUser)
+          .success(function (response) {
+            console.log(response);
+          });
 
-    } else {
-      // create 
-      console.log(this.currentUser.driver);
-      this.myVehicle1 = {};
-      this.myVehicle1.driverId = this.currentUser.driver._id;
-      this.$http.post('/api/vehicles', this.myVehicle1)
-        .success(function(response){
-          console.log(response);
-        });
-    }
+      } else {
+        this.driverId = this.currentUser.driver_id;
+        this.$http.post('/api/users/basicInfoCreat', this.currentUser)
+          .success(function (response) {
+            console.log(response);
+          });
+      }
 
-    if(this.myVehicle2 && this.myVehicle2._id) {
-      // update
-      this.$http.put('/api/vehicles/'+this.myVehicle2._id, this.myVehicle2)
-        .success(function(response){
-          console.log(response);
-        });
+      if (this.myVehicle1) {
+        if (this.myVehicle1._id) {
+          // update
+          this.$http.put('/api/vehicles/' + this.myVehicle1._id, this.myVehicle1)
+            .success(function (response) {
+              console.log(response);
+            });
 
-    } else {
-      // create 
-      console.log(this.currentUser.driver);
-      this.myVehicle2 = {};
-      this.myVehicle2.driverId = this.currentUser.driver._id;
-      this.$http.post('/api/vehicles', this.myVehicle2)
-        .success(function(response){
-          console.log(response);
-        });
+        } else {
+          // create
+          console.log(this.currentUser.driver);
+          this.myVehicle1 = {};
+          this.myVehicle1.driverId = this.currentUser.driver._id;
+          this.$http.post('/api/vehicles', this.myVehicle1)
+            .success(function (response) {
+              console.log(response);
+            });
+        }
+      }
+
+      if (this.myVehicle2) {
+        if (this.myVehicle2 && this.myVehicle2._id) {
+          // update
+          this.$http.put('/api/vehicles/' + this.myVehicle2._id, this.myVehicle2)
+            .success(function (response) {
+              console.log(response);
+            });
+
+        } else {
+          // create
+          console.log(this.currentUser.driver);
+          this.myVehicle2 = {};
+          this.myVehicle2.driverId = this.currentUser.driver._id;
+          this.$http.post('/api/vehicles', this.myVehicle2)
+            .success(function (response) {
+              console.log(response);
+            });
+        }
+      }
+
+      //var _state = this.$state;
+      var modalInstance = this.$modal.open({
+        animation: true,
+        templateUrl: 'myModalContent.html',
+        size: 'sm',
+        controller: function ($state, $modalInstance, $scope) {
+          $scope.ok = function () {
+            $modalInstance.close();
+            $state.reload();
+          };
+        }
+      });
     }
   }
 
-  addThing() {
-    if (this.newThing) {
-      this.$http.post('/api/things', { name: this.newThing });
-      this.newThing = '';
-    }
-  }
 
-  deleteThing(thing) {
-    this.$http.delete('/api/things/' + thing._id);
-  }
-}
-
-
-angular.module('uwece651f16NewApp')
-  .controller('ProfileController', ProfileController);
+  angular.module('uwece651f16NewApp')
+    .controller('ProfileController', ProfileController);
 
 })();
